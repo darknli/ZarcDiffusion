00b9a15ec5845e8ca33c8f06331be51973d29f38
darknli <1187220556@qq.com>
2024-03-25 23:21:05 +0800

    update setup.py +3

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     install_requires=[
-        "Pytorch-Frame @ git+https://github.com/darknli/Pytorch-Frame.git@v1.7.6",
+        "torch-frame @ git+https://github.com/darknli/Pytorch-Frame.git@v1.7.6",
         "pyyaml",
         "Pillow"
     ],
